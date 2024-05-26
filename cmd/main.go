ae0dcbd2b0368227aac17b7a75f3abdc512b28e0
deVamshi <dev.vamshidhar@gmail.com>
2024-05-27 00:30:20 +0530

    implement mongodb driver

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/deVamshi/golang_food_delivery_api/internal/hotel"
+	"github.com/deVamshi/golang_food_delivery_api/pkg/dbcontext"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load env vars
+	APP_ENV, err := godotenv.Read("../.env")
+	if err != nil {
+		log.Fatal(err)
+		log.Fatal("Error loading .env file")
+	}
 
 	r := gin.Default()
 
-	v1 := r.Group("/v1")
-	{
-		hotel.RegisterHandlers(v1, hotel.NewService(hotel.NewRepository("db")))
+	dbClient, err := dbcontext.ConnectDB(APP_ENV["MONGOURI"])
+	defer func() {
+		if err := dbcontext.DisconnectDB(); err != nil {
+			log.Fatal(err)
+		}
+		log.Print("Disconnected to DB\nBye!")
+	}()
+
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
 
-	r.Run("localhost:8080")
+	v1 := r.Group("/v1")
+	{
+		hotel.RegisterHandlers(v1, hotel.NewService(hotel.NewRepository(dbClient)))
+	}
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	// func() {
+	// 	time.Sleep(3 * time.Second)
+	// 	server.Shutdown(context.Background())
+	// }()
+
+	server.ListenAndServe()
+
 }
